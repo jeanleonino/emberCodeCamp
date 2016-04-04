42dff2a0ee5fb8f5300fd9c9d9285f8f7da012ed
Jean Lucas <jeanleonino@gmail.com>
2016-04-04 15:17:28 -0400

    Added basic styles and setup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,10 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf');
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff');
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2');
+  app.import('bower_components/uikit/js/uikit.min.js');
+
   return app.toTree();
 };
